1f101297dc29f541cc846305902ba76abf74de51
Gleb Lebedev <gleb@gleblebedev.com>
2020-10-03 16:53:22 +0100

    ios fixes

--- src/rbfxSample.Shared/ApplicationOptions.cs
+++ src/rbfxSample.Shared/ApplicationOptions.cs
@@ -1,6 +1,7 @@
 ﻿using System.Diagnostics;
+#if !__IOS__
 using CommandLine;
-using Urho3DNet;
+#endif 
 
 namespace rbfxSample
 {
@@ -11,19 +12,29 @@ namespace rbfxSample
             Windowed = Debugger.IsAttached;
         }
 
+#if !__IOS__
         [Option('w', "windowed", Required = false, HelpText = "Windowed mode")] 
+#endif 
         public bool Windowed { get; set; }
 
+#if !__IOS__
         [Option("width", Required = false, HelpText = "Window width")]
+#endif 
         public int? Width { get; set; }
 
+#if !__IOS__
         [Option("height", Required = false, HelpText = "Window height")]
+#endif 
         public int? Height { get; set; }
 
+#if !__IOS__
         [Option('h', "highdpi", Required = false, HelpText = "High DPI mode")]
+#endif 
         public bool HighDpi { get; set; } = false;
 
+#if !__IOS__
         [Option("renderpath", Required = false, HelpText = "Render path")]
+#endif 
         public string RenderPath { get; set; } = "RenderPaths/ForwardHWDepth.xml";
     }
 }
