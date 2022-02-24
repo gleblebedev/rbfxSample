c945e0df7287a945df70617ae5bbf3be00c7ac96
Gleb Lebedev <gleb@gleblebedev.com>
2022-02-24 22:51:05 +0000

    0.3.7.331

--- src/rbfxSample.Android/Resources/Resource.designer.cs
+++ src/rbfxSample.Android/Resources/Resource.designer.cs
@@ -14,7 +14,7 @@ namespace rbfxSample
 {
 	
 	
-	[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Android.Build.Tasks", "1.0.0.0")]
+	[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Android.Build.Tasks", "12.2.99.54")]
 	public partial class Resource
 	{
 		
